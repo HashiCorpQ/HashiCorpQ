8a445af725ff8475753357a68203a7c605c54f3b
nQ <iam.administrator+HashiCorpQ@webiq.cloud>
2026-07-02 18:35:52 -0400

    Format for CI

--- live/infrastructure/foundation/outputs.tf
+++ live/infrastructure/foundation/outputs.tf
@@ -1,4 +1,4 @@
-output "account_id"         { value = data.aws_caller_identity.current.account_id }
-output "artifacts_bucket"   { value = module.artifacts_bucket.bucket_id }
+output "account_id" { value = data.aws_caller_identity.current.account_id }
+output "artifacts_bucket" { value = module.artifacts_bucket.bucket_id }
 output "ssm_parameter_name" { value = aws_ssm_parameter.greeting.name }
-output "budget_name"        { value = aws_budgets_budget.monthly.name }
+output "budget_name" { value = aws_budgets_budget.monthly.name }
